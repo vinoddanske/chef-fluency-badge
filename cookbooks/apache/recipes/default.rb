0d69e9fe20498f58e129544779d09068447deb36
Vinod <root@localhost>
2017-07-18 09:10:52 +0000

    added motd

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -3,11 +3,16 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+if node['platform_family'] == "rhel"
+	package = "httpd"
+elsif node['platform_family'] == "debian"
+end
 package 'apache2' do
-	package_name 'httpd'
+	package_name package
 	action :install
 end 
 service 'apache2' do
 	service_name 'httpd'
 	action [:start, :enable]
 end
+
